316eda0721a72ca2f284de2a497080f4064f8e6c
Ian Macalinao <me@ian.pw>
2016-08-06 22:22:28 -0400

    some sidebar

--- viz/src/App.js
+++ viz/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
 import Transcript from './Transcript';
 
 const events = [
@@ -34,6 +35,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Sidebar />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
---
+++ viz/src/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Sidebar.css';
+
+const interviews = [
+  {
+    type: 'Distributed Systems',
+    interviewer: 'Christina Huang',
+    selected: true,
+  },
+  {
+    type: 'Algorithms',
+    interviewer: 'Jeff Dean',
+  },
+  {
+    type: 'Algorithms',
+    interviewer: 'Sanjay Gupta',
+  },
+]
+
+export default () => (
+  <div className="Sidebar">
+    <div id="logoContainer">
+      <img id="logo" src="./images/logo.png" />
+    </div>
+    <h2>Candidate Interviews</h2>
+    <ul>
+      {
+        interviews.map((interview) =>
+          <li className={ interview.selected && 'selected' }>
+            <h3>{ interview.type }</h3>
+            <h4>{ interview.interviewer }</h4>
+          </li>)
+      }
+    </ul>
+    <h2>Related Candidates</h2>
+  </div>
+)
