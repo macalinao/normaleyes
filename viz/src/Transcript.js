bf20b9724e3ba93ca2b4af60686a1ea13fa9acce
Ian Macalinao <me@ian.pw>
2016-08-07 08:44:01 -0400

    Some styles

--- viz/src/Transcript.js
+++ viz/src/Transcript.js
@@ -5,13 +5,15 @@ export default ({ events, currentEvent, onTranscriptSelect }) => {
 
   return (
     <div className="Transcript">
+      <ul>
       {
         events.map((event, i) => (
           <li className={ i === currentEvent && 'Transcript-current' }>
-            <a onClick={() => onTranscriptSelect(i)}>{ event.speaker }: { event.text }</a>
+            <a onClick={() => onTranscriptSelect(i)}><em>({ event.time }s)</em> <strong>{ event.speaker }:</strong> { event.text }</a>
           </li>
         ))
       }
+      </ul>
     </div>
   );
 
